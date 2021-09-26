bcb1aa8dc03a7026795b8ca4800688496edaafdd
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-26 03:33:59 +0200

    Add video detail component

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import parse from 'html-react-parser';
+
+const VideoDetail = ( props ) => {
+  const { video } = props;
+
+  if ( !video ) {
+    return <div>Loading...</div>;
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${ video.id.videoId }`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe title="video player" src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{parse( video.snippet.title )}</h4>
+        <p>{parse( video.snippet.description )}</p>
+      </div>
+    </div>
+  );
+}
+
+export default VideoDetail;
