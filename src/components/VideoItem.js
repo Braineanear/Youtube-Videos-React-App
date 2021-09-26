4f0e7d441686a55f31fe14182dded62adcf66292
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-09-26 03:34:23 +0200

    Add video item component

---
+++ src/components/VideoItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import parse from 'html-react-parser';
+
+import './VideoItem.css';
+
+const VideoItem = ( props ) => {
+  const { video, onVideoSelect } = props;
+
+  return (
+    <div onClick={() => onVideoSelect( video )} className='video-item item'>
+      <img
+        alt={parse( video.snippet.title )}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{parse( video.snippet.title )}</div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoItem;
